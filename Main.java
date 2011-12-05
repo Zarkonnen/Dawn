c0408b4903b3dc75e82cf6b3eb223ea37126268f
David Stark <david.stark@zarkonnen.com>
2011-12-05 20:39:08 +0100

    Map display and WASD navigation for B.

--- Main.java
+++ Main.java
@@ -13,6 +13,42 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 	MouseEvent click = null;
 	BufferStrategy strategy;
 	
+	static final int TILE_SIZE = 40;
+	static final int T_W = 19;
+	static final int T_H = 15;
+	
+	// Tile types
+	static final byte _ = 0;
+	static final byte W = 1;
+	
+	// b stats
+	static final double B_RUN_SPEED = 0.1;
+	double b_x = 0;
+	double b_y = 0;
+	
+	// v stats
+	double v_x = 0;
+	double v_y = 0;
+	int[][] v_map = new int[T_H][T_W];
+	
+	byte[][] t_type = {
+		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, W, W, W, W, W, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, W, W, W, _, W, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, W, W, W, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, W, W, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _}
+	};
+	
 	@Override
 	public void init() {
 		setIgnoreRepaint(true);
@@ -26,34 +62,58 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 		new Thread(this).start();
 	}
 
+	@Override
 	public void run() {
 		while (true) {
+			// Input
+			if (key[KeyEvent.VK_W]) { b_y -= B_RUN_SPEED; b_y = b_y < 0 ? 0 : b_y; }
+			if (key[KeyEvent.VK_S]) { b_y += B_RUN_SPEED; b_y = b_y > (T_H - 1) ? (T_H - 1) : b_y; }
+			if (key[KeyEvent.VK_A]) { b_x -= B_RUN_SPEED; b_x = b_x < 0 ? 0 : b_x; }
+			if (key[KeyEvent.VK_D]) { b_x += B_RUN_SPEED; b_x = b_x > (T_W - 1) ? (T_W - 1) : b_x; }
+			
+			// Graphics
 			Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
-			g.setColor(Color.BLACK);
-			g.fillRect(0, 0, 800, 600);
-			if (click != null) {
-				g.setColor(Color.RED);
-				g.fillRect(click.getX(), click.getY(), 100, 100);
-			}
+			for (int y = 0; y < T_H; y++) { for (int x = 0; x < T_W; x++) {
+				Color c = null;
+				switch (t_type[y][x]) {
+					case _: c = Color.LIGHT_GRAY; break;
+					case W: c = Color.DARK_GRAY;  break;
+				}
+				g.setColor(c);
+				g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}}
+			g.setColor(Color.RED);
+			g.fillOval((int) (v_x * TILE_SIZE), (int) (v_y * TILE_SIZE), TILE_SIZE, TILE_SIZE);
+			g.setColor(Color.GREEN);
+			g.fillOval((int) (b_x * TILE_SIZE), (int) (b_y * TILE_SIZE), TILE_SIZE, TILE_SIZE);
 			strategy.show();
+			try { Thread.sleep(25); } catch (Exception e) {}
 		}
 	}
 
+	@Override
 	public void keyTyped(KeyEvent e) {}
 
+	@Override
 	public void keyPressed(KeyEvent e) {
 		key[((KeyEvent) e).getKeyCode()] = true;
 	}
 
+	@Override
 	public void keyReleased(KeyEvent e) {
 		key[((KeyEvent) e).getKeyCode()] = false;
 	}
 
+	@Override
 	public void mouseClicked(MouseEvent e) {}
+	@Override
 	public void mousePressed(MouseEvent e) {
 		click = e;
 	}
+	@Override
 	public void mouseReleased(MouseEvent e) {}
+	@Override
 	public void mouseEntered(MouseEvent e) {}
+	@Override
 	public void mouseExited(MouseEvent e) {}
 }
