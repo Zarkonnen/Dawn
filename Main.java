82c240d0d672227cd2e8cd1c0874061ae583db05
David Stark <david.stark@zarkonnen.com>
2011-12-06 07:57:30 +0100

    Doors, walls, following, breaking through.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
@@ -24,17 +25,25 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 	static final int[] X_DIRS = { 0, 0, -1, 1};
 	static final int[] Y_DIRS = { -1, 1, 0, 0};
 	
+	String msg = "";
+	
 	// Tile types
 	static final byte _ = 0;
 	static final byte W = 1;
+	static final byte O = 2;
+	static final byte D = 3;
 	
 	// b stats
 	static final double B_RUN_SPEED = 0.1;
+	static final int B_COOLDOWN = 20;
+	int b_cooldown = 0;
 	double b_x = 0;
 	double b_y = 0;
 	
 	// v stats
-	static final double V_SPEED = 0.01;
+	static final double V_SPEED = 0.05;
+	static final int V_COOLDOWN = 30;
+	int v_cooldown = 0;
 	double v_x = 0;
 	double v_y = 0;
 	int[][] v_map = new int[T_H][T_W];
@@ -42,7 +51,9 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 	// map
 	static byte[] T_TO_HP = {
 		0, // _
-		10 // W
+		20,// W
+		0, // O
+		10,// D
 	};
 	
 	byte[][] t_type = {
@@ -53,13 +64,13 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
-		{_, _, _, W, W, W, W, _, W, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, W, W, W, W, O, W, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, W, W, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
-		{_, _, _, _, _, _, _, W, W, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, _, W, W, O, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _}
 	};
 	byte[][] t_hp = new byte[T_H][T_W];
@@ -83,8 +94,10 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 		for (int y = 0; y < T_H; y++) { for (int x = 0; x < T_W; x++) {
 			t_hp[y][x] = T_TO_HP[t_type[y][x]];
 		}}
-		
+				
 		while (true) {
+			b_cooldown--;
+			v_cooldown--;
 			// Input
 			if (key[KeyEvent.VK_W]) {
 				b_y -= B_RUN_SPEED;
@@ -113,6 +126,33 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 				b_x = b_x > (T_W - P_R) ? (T_W - P_R) : b_x;
 				// Solid things
 				b_x = t_hp[(int) b_y][(int) b_x] > 0 ? b_x - B_RUN_SPEED : b_x;
+			}
+			
+			// Interaction
+			msg = "";
+
+			for (int i = 0; i < 4; i++) {
+				int ny = ((int) b_y) + Y_DIRS[i];
+				int nx = ((int) b_x) + X_DIRS[i];
+				if (nx < 0 || ny < 0 || nx >= T_W || ny >= T_H) { continue; }
+				switch (t_type[ny][nx]) {
+					case O:
+						msg = "Press space to lock door.";
+						if (b_cooldown <= 0 && key[KeyEvent.VK_SPACE]) {
+							t_type[ny][nx] = D;
+							t_hp[ny][nx] = T_TO_HP[D];
+							b_cooldown = B_COOLDOWN;
+						}
+						break;
+					case D:
+						msg = "Press space to open door.";
+						if (b_cooldown <= 0 && key[KeyEvent.VK_SPACE]) {
+							t_type[ny][nx] = O;
+							t_hp[ny][nx] = T_TO_HP[O];
+							b_cooldown = B_COOLDOWN;
+						}
+						break;
+				}
 			}
 			
 			// V-map update
@@ -160,11 +200,24 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 			}
 			if (dir != -1) {
 				v_y += Y_DIRS[dir] * V_SPEED;
+				// Borders
 				v_y = v_y < P_R ? P_R : v_y;
 				v_y = v_y > (T_H - P_R) ? (T_H - P_R) : v_y;
 				v_x += X_DIRS[dir] * V_SPEED;
 				v_x = v_x < P_R ? P_R : v_x;
 				v_x = v_x > (T_W - P_R) ? (T_W - P_R) : v_x;
+				// Walls
+				if (t_hp[(int) v_y][(int) v_x] > 0) {
+					if (v_cooldown <= 0) {
+						t_hp[(int) v_y][(int) v_x]--;
+						v_cooldown = V_COOLDOWN;
+						if (t_hp[(int) v_y][(int) v_x] == 0) {
+							t_type[(int) v_y][(int) v_x] = _;
+						}
+					}
+					v_y -= Y_DIRS[dir] * V_SPEED;
+					v_x -= X_DIRS[dir] * V_SPEED;
+				}
 			}
 			
 			// Graphics
@@ -174,6 +227,8 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 				switch (t_type[y][x]) {
 					case _: c = Color.LIGHT_GRAY; break;
 					case W: c = Color.DARK_GRAY;  break;
+					case O: c = Color.GRAY;       break;
+					case D: c = new Color(127, 127, 0); break;
 				}
 				g.setColor(c);
 				g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
@@ -184,6 +239,9 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 			g.fillOval((int) ((v_x - P_R) * TILE_SIZE), (int) ((v_y - P_R) * TILE_SIZE), (int) (2 * P_R * TILE_SIZE), (int) (2 * P_R * TILE_SIZE));
 			g.setColor(Color.GREEN);
 			g.fillOval((int) ((b_x - P_R) * TILE_SIZE), (int) ((b_y - P_R) * TILE_SIZE), (int) (2 * P_R * TILE_SIZE), (int) (2 * P_R * TILE_SIZE));
+			g.setFont(new Font("Verdana", Font.PLAIN, 20));
+			g.setColor(Color.WHITE);
+			g.drawString(msg, 40, 300);
 			strategy.show();
 			try { Thread.sleep(25); } catch (Exception e) {}
 		}
