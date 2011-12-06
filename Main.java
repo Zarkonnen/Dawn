afed8e1588e43e3d47ecd7373cd19413b54db477
David Stark <david.stark@zarkonnen.com>
2011-12-06 11:46:56 +0100

    Improved v behaviour. Added exhaustion, lose condition.

--- Main.java
+++ Main.java
@@ -22,37 +22,49 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 	static final int T_H = 15;
 	static final double P_R = 0.3;
 	
-	static final int[] X_DIRS = { 0, 0, -1, 1};
-	static final int[] Y_DIRS = { -1, 1, 0, 0};
+	static final int[] X_DIRS = { 0, 0, -1, 1 , -1, 1, 1,-1};
+	static final int[] Y_DIRS = { -1, 1, 0, 0 , -1,-1, 1, 1};
 	
 	String msg = "";
 	
 	// Tile types
 	static final byte _ = 0;
-	static final byte W = 1;
-	static final byte O = 2;
+	static final byte O = 1;
+	static final byte W = 2; static final int SOLIDS = 2;
 	static final byte D = 3;
 	
 	// b stats
 	static final double B_RUN_SPEED = 0.1;
+	static final double B_WALK_SPEED = 0.03;
 	static final int B_COOLDOWN = 20;
 	int b_cooldown = 0;
-	double b_x = 0;
-	double b_y = 0;
+	int b_fatigue = 0;
+	int b_exhaustion = 0;
+	double b_x = 14;
+	double b_y = 7;
 	
 	// v stats
 	static final double V_SPEED = 0.05;
 	static final int V_COOLDOWN = 30;
 	int v_cooldown = 0;
-	double v_x = 0;
-	double v_y = 0;
+	double v_x = 11;
+	double v_y = 9;
 	int[][] v_map = new int[T_H][T_W];
+	
+	// bang
+	int bang_tick = 0;
+	int bang_x = -1;
+	int bang_y = -1;
+	
+	// game
+	boolean v_vs_b = false;
+	boolean game_over = false;
 	
 	// map
 	static byte[] T_TO_HP = {
 		0, // _
+		10, // O
 		20,// W
-		0, // O
 		10,// D
 	};
 	
@@ -61,13 +73,13 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, W, W, W, W, W, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
+		{_, _, _, O, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
 		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
-		{_, _, _, W, _, _, _, _, _, W, _, _, _, _, _, _, _, _, _},
-		{_, _, _, W, W, W, W, O, W, W, _, _, _, _, _, _, _, _, _},
-		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
-		{_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
-		{_, _, _, _, _, _, _, W, W, W, W, _, _, _, _, _, _, _, _},
+		{_, _, _, W, W, W, W, O, W, W, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, W, _, _, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, W, _, _, _, W, _, _, _, _, _, _, _, _},
+		{_, _, _, _, _, _, W, W, D, W, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, _, _, W, _, _, _, _, _, _, _, _},
 		{_, _, _, _, _, _, _, W, W, O, W, _, _, _, _, _, _, _, _},
@@ -98,34 +110,48 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 		while (true) {
 			b_cooldown--;
 			v_cooldown--;
+			double sp = b_fatigue > 400 ? B_WALK_SPEED : B_RUN_SPEED;
+			boolean mv = false;
 			// Input
-			if (key[KeyEvent.VK_W]) {
-				b_y -= B_RUN_SPEED;
+			if (key[KeyEvent.VK_UP]) {
+				b_y -= sp;
 				// Borders
 				b_y = b_y < P_R ? P_R : b_y;
 				// Solid things
-				b_y = t_hp[(int) b_y][(int) b_x] > 0 ? b_y + B_RUN_SPEED : b_y;
+				b_y = t_type[(int) b_y][(int) b_x] >= SOLIDS ? b_y + sp : b_y;
+				mv = true;
 			}
-			if (key[KeyEvent.VK_S]) {
-				b_y += B_RUN_SPEED;
+			if (key[KeyEvent.VK_DOWN]) {
+				b_y += sp;
 				// Borders
 				b_y = b_y > (T_H - P_R) ? (T_H - P_R) : b_y;
 				// Solid things
-				b_y = t_hp[(int) b_y][(int) b_x] > 0 ? b_y - B_RUN_SPEED : b_y;
+				b_y = t_type[(int) b_y][(int) b_x] >= SOLIDS ? b_y - sp : b_y;
+				mv = true;
 			}
-			if (key[KeyEvent.VK_A]) {
-				b_x -= B_RUN_SPEED;
+			if (key[KeyEvent.VK_LEFT]) {
+				b_x -= sp;
 				// Borders
 				b_x = b_x < P_R ? P_R : b_x;
 				// Solid things
-				b_x = t_hp[(int) b_y][(int) b_x] > 0 ? b_x + B_RUN_SPEED : b_x;
+				b_x = t_type[(int) b_y][(int) b_x] >= SOLIDS ? b_x + sp : b_x;
+				mv = true;
 			}
-			if (key[KeyEvent.VK_D]) {
-				b_x += B_RUN_SPEED;
+			if (key[KeyEvent.VK_RIGHT]) {
+				b_x += sp;
 				// Borders
 				b_x = b_x > (T_W - P_R) ? (T_W - P_R) : b_x;
 				// Solid things
-				b_x = t_hp[(int) b_y][(int) b_x] > 0 ? b_x - B_RUN_SPEED : b_x;
+				b_x = t_type[(int) b_y][(int) b_x] >= SOLIDS ? b_x - sp : b_x;
+				mv = true;
+			}
+			
+			if (mv && b_fatigue <= 600) {
+				b_fatigue++;
+				b_exhaustion++;
+			}
+			if (!mv && b_fatigue > b_exhaustion / 50) {
+				b_fatigue--;
 			}
 			
 			// Interaction
@@ -140,7 +166,6 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 						msg = "Press space to lock door.";
 						if (b_cooldown <= 0 && key[KeyEvent.VK_SPACE]) {
 							t_type[ny][nx] = D;
-							t_hp[ny][nx] = T_TO_HP[D];
 							b_cooldown = B_COOLDOWN;
 						}
 						break;
@@ -148,7 +173,6 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 						msg = "Press space to open door.";
 						if (b_cooldown <= 0 && key[KeyEvent.VK_SPACE]) {
 							t_type[ny][nx] = O;
-							t_hp[ny][nx] = T_TO_HP[O];
 							b_cooldown = B_COOLDOWN;
 						}
 						break;
@@ -189,61 +213,106 @@ public class Main extends JApplet implements Runnable, KeyListener, MouseListene
 			// V movement
 			int dir = -1;
 			int least = v_map[((int) v_y)][((int) v_x)];
-			for (int i = 0; i < 4; i++) {
+			for (int i = 0; i < 8; i++) {
 				int ny = ((int) v_y) + Y_DIRS[i];
 				int nx = ((int) v_x) + X_DIRS[i];
+				int ny2 = (int) (v_y + Y_DIRS[i] * V_SPEED);
+				int nx2 = (int) (v_x + X_DIRS[i] * V_SPEED);
 				if (nx < 0 || ny < 0 || nx >= T_W || ny >= T_H) { continue; }
-				if (v_map[ny][nx] < least) {
+				int value = v_map[ny][nx];
+				if (ny2 != (int) v_y || nx2 != (int) v_x) {
+					value = Math.max(value, v_map[ny2][nx2]);
+				}
+				if (value < least) {
 					dir = i;
-					least = v_map[ny][nx];
+					least = value;
 				}
 			}
-			if (dir != -1) {
-				v_y += Y_DIRS[dir] * V_SPEED;
-				// Borders
-				v_y = v_y < P_R ? P_R : v_y;
-				v_y = v_y > (T_H - P_R) ? (T_H - P_R) : v_y;
-				v_x += X_DIRS[dir] * V_SPEED;
-				v_x = v_x < P_R ? P_R : v_x;
-				v_x = v_x > (T_W - P_R) ? (T_W - P_R) : v_x;
-				// Walls
-				if (t_hp[(int) v_y][(int) v_x] > 0) {
-					if (v_cooldown <= 0) {
-						t_hp[(int) v_y][(int) v_x]--;
-						v_cooldown = V_COOLDOWN;
-						if (t_hp[(int) v_y][(int) v_x] == 0) {
-							t_type[(int) v_y][(int) v_x] = _;
-						}
+			double dy = 0;
+			double dx = 0;
+			double dist = Math.sqrt((b_y - v_y) * (b_y - v_y) + (b_x - v_x) * (b_x - v_x));
+			if (dir == -1) {
+				if (dist > P_R) {
+					dy = (b_y - v_y) / dist * V_SPEED;
+					dx = (b_x - v_x) / dist * V_SPEED;
+				}
+			} else {
+				dy = Y_DIRS[dir] * V_SPEED;
+				dx = X_DIRS[dir] * V_SPEED;;
+			}
+			v_y += dy;
+			v_y = v_y < P_R ? P_R : v_y;
+			v_y = v_y > (T_H - P_R) ? (T_H - P_R) : v_y;
+			v_x += dx;
+			v_x = v_x < P_R ? P_R : v_x;
+			v_x = v_x > (T_W - P_R) ? (T_W - P_R) : v_x;
+			// Walls
+			if (t_type[(int) v_y][(int) v_x] >= SOLIDS) {
+				if (v_cooldown <= 0) {
+					t_hp[(int) v_y][(int) v_x]--;
+					bang_tick = 10;
+					bang_y = (int) v_y;
+					bang_x = (int) v_x;
+					v_cooldown = V_COOLDOWN;
+					if (t_hp[(int) v_y][(int) v_x] == 0) {
+						t_type[(int) v_y][(int) v_x] = _;
 					}
-					v_y -= Y_DIRS[dir] * V_SPEED;
-					v_x -= X_DIRS[dir] * V_SPEED;
+				}
+				v_y -= dy;
+				v_x -= dx;
+			}
+			
+			v_vs_b = false;
+			if (dist < P_R * 2) {
+				b_fatigue += 7;
+				v_vs_b = true;
+				if (b_fatigue >= 600) {
+					game_over = true;
+					msg = "GAME OVER";
 				}
 			}
 			
 			// Graphics
 			Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
-			for (int y = 0; y < T_H; y++) { for (int x = 0; x < T_W; x++) {
-				Color c = null;
-				switch (t_type[y][x]) {
-					case _: c = Color.LIGHT_GRAY; break;
-					case W: c = Color.DARK_GRAY;  break;
-					case O: c = Color.GRAY;       break;
-					case D: c = new Color(127, 127, 0); break;
-				}
-				g.setColor(c);
-				g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-				g.setColor(Color.BLACK);
-				g.drawString("" + v_map[y][x], x * TILE_SIZE + 10, y * TILE_SIZE + 20);
-			}}
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, 800, 600);
+			if (!game_over) {
+				for (int y = 0; y < T_H; y++) { for (int x = 0; x < T_W; x++) {
+					Color c = null;
+					switch (t_type[y][x]) {
+						case _: c = Color.LIGHT_GRAY; break;
+						case W: c = Color.DARK_GRAY;  break;
+						case O: c = Color.GRAY;       break;
+						case D: c = new Color(127, 127, 0); break;
+					}
+					g.setColor(c);
+					g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+					/*g.setColor(Color.BLACK);
+					g.drawString("" + v_map[y][x], x * TILE_SIZE + 10, y * TILE_SIZE + 20);*/
+				}}
+			}
 			g.setColor(Color.RED);
 			g.fillOval((int) ((v_x - P_R) * TILE_SIZE), (int) ((v_y - P_R) * TILE_SIZE), (int) (2 * P_R * TILE_SIZE), (int) (2 * P_R * TILE_SIZE));
-			g.setColor(Color.GREEN);
+			if (!(v_vs_b && b_fatigue % 19 == 0)) { g.setColor(Color.GREEN); }
 			g.fillOval((int) ((b_x - P_R) * TILE_SIZE), (int) ((b_y - P_R) * TILE_SIZE), (int) (2 * P_R * TILE_SIZE), (int) (2 * P_R * TILE_SIZE));
-			g.setFont(new Font("Verdana", Font.PLAIN, 20));
+			g.setColor(Color.YELLOW);
+			if (bang_tick-- > 0) {
+				g.fillOval(bang_x * TILE_SIZE, bang_y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}
+			g.fillRect(760, 600 - b_fatigue / 4, 40, b_fatigue / 4);
+			if (b_fatigue > 400) {
+				g.setColor(Color.ORANGE);
+				g.fillRect(760, 600 - b_fatigue / 4, 40, (b_fatigue - 400) / 4);
+			}
 			g.setColor(Color.WHITE);
+			g.fillRect(760, 600 - b_exhaustion / 50, 40, b_exhaustion / 50);
+			g.setFont(new Font("Verdana", Font.PLAIN, 20));
 			g.drawString(msg, 40, 300);
 			strategy.show();
 			try { Thread.sleep(25); } catch (Exception e) {}
+			if (game_over) {
+				return;
+			}
 		}
 	}
 
